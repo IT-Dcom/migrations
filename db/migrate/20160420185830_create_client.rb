3c0bc35fd96167ab3711a4a95ff10e07c8be1196
MaximeRigaud <maxime.rigaud.mail@gmail.com>
2016-04-20 19:31:11 +0200

    Create 20160420185830_create_client.rb

---
+++ db/migrate/20160420185830_create_client.rb
@@ -0,0 +1,11 @@
+ass CreateClient < ActiveRecord::Migration
+  def change
+      create_table :clients do |t|
+      t.datetime :name
+      t.datetime :email
+      t.datetime :address
+      t.datetime :zip
+      t.timestamps null: false
+    end
+  end
+end
