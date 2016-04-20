fbd99d56d2afe4ae181f2b5c9754d337cfb97399
MaximeRigaud <maxime.rigaud.mail@gmail.com>
2016-04-20 19:29:37 +0200

    Create 20160420184950_create_insurance.rb

---
+++ db/migrate/20160420184950_create_insurance.rb
@@ -0,0 +1,9 @@
+ass CreateInsurance < ActiveRecord::Migration
+  def change
+      create_table :insurances do |t|
+      t.datetime :name
+      t.datetime :price
+      t.timestamps null: false
+    end
+  end
+end
