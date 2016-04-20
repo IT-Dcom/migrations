f222854171317fc9d8b2ed49a9451fa27e7cad87
MaximeRigaud <maxime.rigaud.mail@gmail.com>
2016-04-20 19:28:02 +0200

    Create 20160420184848_create_restaurant.rb

---
+++ db/migrate/20160420184848_create_restaurant.rb
@@ -0,0 +1,9 @@
+ass CreateRestaurant < ActiveRecord::Migration
+  def change
+      create_table :restaurants do |t|
+      t.datetime :address
+      t.datetime :price
+      t.timestamps null: false
+    end
+  end
+end
