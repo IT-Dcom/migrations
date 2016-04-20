8d82c66d5df897393ea583a781ec421e210fd84f
MaximeRigaud <maxime.rigaud.mail@gmail.com>
2016-04-20 19:28:47 +0200

    Create 20160420184900_create_driver.rb

---
+++ db/migrate/20160420184900_create_driver.rb
@@ -0,0 +1,10 @@
+ass CreateDriver < ActiveRecord::Migration
+  def change
+      create_table :drivers do |t|
+      t.datetime :first_name
+      t.datetime :last_name
+      t.datetime :address
+      t.timestamps null: false
+    end
+  end
+end
